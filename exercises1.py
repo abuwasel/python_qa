b6aa1d2d3cc2231fea0701ce02c5f39605624489
Ibrahim Abu Wasel <i.abuwasel@gmail.com>
2023-07-19 13:51:29 +0300

    few changes

--- exercises1.py
+++ exercises1.py
@@ -1,30 +1,30 @@
-#1
+#1 - מתי כדאי להשתמש ב range בלולאת for?
 
 
-#2
+#2 - כיצד אפשר להגדיר range עם קפיצות? כיצד אפשר להגדיר range עם מספרים בסדר יורד?
 print(list(range(0,10,2)))
 lists = list(range(10))
 lists.reverse()
 print(lists)
 
-#3
+#3 - כיצד אפשר לייצר list מתוך range?
 print(list(range(1,10)))
 
-#4
-#5
-#6
-#7
+#4 - מדוע יש צורך בלולאות מסוג while ו- while-do?
+#5 - מה ההבדל בין לולאת while לבין לולאת while-do?
+#6 - כיצד אפשר בפייטון לממש לולאת while-do?
+#7 -  מה ההבדל בין break לבין continue ?מדוע כדאי להשתמש ב- continue?
 
-#8
+#8 -  כתוב תוכנית המדפיסה את כל המספרים השלמים מ- 200 ועד 2 בסדר יורד
 list_down = list(range(2,201))
 list_down.reverse()
 print(list_down)
 
-#9
+#9 -  כתוב תוכנית המדפיסה את כל הכפולות של ,7 בטווח שבין 1 ל- 100 (נסה לכתוב בצורה יעילה)
 for x in range(1,101):
     print(f'{x} x 7 = {x*7}')
 
-#10
+#10 - כתוב תוכנית הקולטת מספרים עד אשר נקלט מספר שלילי. הדפס את סכום המספרים שנקלט (ללא המספר השלילי)
 sum=0
 while True:
     number = int(input('Please Enter Number: '))
@@ -33,7 +33,7 @@ while True:
     sum += number
 print(f'The sum of the numbers is: {sum}')
 
-#11
+#11 - כתוב תוכנית הקולטת מספר ומדפיסה את העצרת שלו.
 number = int(input('Please Enter Number: '))
 multi = 1
 for num in range(1,number+1):
