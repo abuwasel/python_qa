daaabd72707f02175decb6c0b9303d8ac66655b7
Ibrahim Abu Wasel <i.abuwasel@gmail.com>
2023-07-21 18:22:28 +0300

    few changes

--- dict-exercise.py
+++ dict-exercise.py
@@ -1,3 +1,34 @@
+user = {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+    }
+}
+
+user = user['company']['name']
+if user.startswith('R'):
+    print('yes')
+else:
+    print('no')
+
+
 d1 = {'a':155}
 d2 = {'b':188}
 d1.update(d2) #update() adds one dict item to aniother
